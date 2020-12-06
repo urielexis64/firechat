c362a2e3effdda67c7a93819f8856af055d9b886
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-05 23:00:10 -0700

    Observable listening in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'firechat';
+  public chat$: Observable<any[]>;
+  constructor(firestore: AngularFirestore) {
+    this.chat$ = firestore.collection('chats').valueChanges();
+  }
 }
