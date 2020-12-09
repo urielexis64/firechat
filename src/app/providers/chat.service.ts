0a9bf07ce0be339ff8bb8a554b12f04cf354cfd8
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-08 20:32:52 -0700

    Login and logout with Google

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -5,14 +5,38 @@ import {
 } from 'angularfire2/firestore';
 import { Message } from '../interfaces/message.interface';
 import { map } from 'rxjs/operators';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
   private itemsCollection: AngularFirestoreCollection<Message>;
-  chats: Message[] = [];
-  constructor(private afs: AngularFirestore) {}
+  public chats: Message[] = [];
+  public user: any = {};
+
+  constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe((user) => {
+      console.log(user);
+
+      if (user) {
+        this.user.name = user.displayName;
+        this.user.uid = user.uid;
+      }
+    });
+  }
+
+  login(provider: string) {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(() => {
+        console.log('Logged successfully');
+      });
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 
   loadMessages() {
     this.itemsCollection = this.afs.collection<Message>('chats', (ref) =>
